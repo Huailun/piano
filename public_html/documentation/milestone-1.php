1f59c4d2c6943c216d87dd8715a6c438c91595ce
Ellen Liu <huailun@hotmail.com>
2016-04-18 13:59:33 -0600

    PWP

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,19 +5,18 @@
 		<title>piano</title>
 	</head>
 	<body>
-		<header>
-			<h1>Bach gave us God's word.Mozart gave us God's laughter.Beethoven gave us God's fire.God gave us Music that we might pray without words. </h1>
-			<img src="images/images.png">
-		</header>
+		<header><h1>Bach gave us God's word.Mozart gave us God's laughter.Beethoven gave us God's fire.God gave us Music that we might pray without words. </h1>
+			<img src="images/images.png"></header>
 		<h2>How to have fun with piano</h2>
-		<footer>
+		<section>
 			<img src="images/imagesHAEPXJPK.jpg">
-		</footer>
 		<h2>Must Knows</h2>
 		<ul>
 			<li>How to number your fingers</li>
 			<li>What to read between lines & spaces</li>
+			<li>When is the best time to start</li>
+			<li>Useful links</li>
 		</ul>
-
+		</section>
 	</body>
 </html>
