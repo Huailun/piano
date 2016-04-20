f90f88cb54ed0e8e4c757211c3f64e50966b535f
Ellen Liu <huailun@hotmail.com>
2016-04-20 13:05:42 -0600

    PWP

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -32,6 +32,8 @@
 				<p>Age: 44</p>
 				<p>Status: David Wang and his wife both have full jobs. While they are busy with their own works, they still need to take care of their three young children. They have heard of many good benefits for their children to have piano lessons. But for their busy working schedule, they only have few minutes at night after their children go to bed to do the research.</p>
 			   <p>Goal: This website can provide key information of how to start up, basic music theory and how to apply it on keyboards in a fast and effective way.</p>
+			<h2>Use Case</h2>
+			<p>David wang and his wife have been doing researches online for a while to find a way to take the first step of their children's piano lessons. The amount of information is huge. For they only have very little knowledge about piano and music, the overwhelming information makes it very difficult for them to make a final decision. When they find this website, because it has designed for absolute beginners and parents or grand parents who have no music background to take the first step. They fell this is a useful website for them.</p>
 		</footer>
 	</body>
 </html>
