60a2d9de7db0ed90761dacbc2626af86780d3b2e
Ellen Liu <huailun@hotmail.com>
2016-05-01 17:25:36 -0600

    PWP

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,6 +5,16 @@
 		<title>milestone-2</title>
 	</head>
 	<body>
+		<section>
+			<h1>Proposed Site Map</h1>
+				<p>This site will consist of 4 top-level pages. Besides, there will be a user feedback section(Send Me Message) on the bottom of every page.</p>
+				<ul>
+					<li>About Me</li>
+					<li>About Piano</li>
+					<li>About Music Theory</li>
+					<li>Resources</li>
+				</ul>
+		</section>
 		<h1>Content Strategy</h1>
 
 	</body>
