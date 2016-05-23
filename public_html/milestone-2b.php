ba59c45975d4edbdaace67a040dc5da8435b54ce
Ellen Liu <huailun@hotmail.com>
2016-05-22 18:28:47 -0600

    pwp2b

---
+++ public_html/milestone-2b.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- sets IE rendering to IE-EDGE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<!-- sets viewport width to device width, scaling 1:1 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+
+		<!-- OUR CUSTOM CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<title>PWP</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter"></div>
+
+		<-- header -->
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<!-- brand and toggle get grouped for better mobile display-->
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar">About me</span>
+						<span class="icon-bar">About piano</span>
+						<span class="icon-bar">Some music basics</span>
+						<span class="icon-bar">Link to useful resources</span>
+					</button>
+				</nav>
+			</div>
+		</header>
+		<!-- welcome section -->
+		<section = id="welcome">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					image
+				</div>
+
+
+			</div>
+		</div>
+		</section>
+
+		<!-- thumbnails -->
+		<section id="thumbnails">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="thumbnail">
+							<img src="http:://placekitten.com/g/100/100" alt="" class="image">
+							<div class="caption">
+								<h3>Thumbnail label</h3>
+								<p>whatever</p>
+								<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+							</div>
+						</div>
+
+
+					</div>
+
+
+	</Body>
