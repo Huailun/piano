344db1c89f8aadbdd406949d0e69920ca3f83924
Ellen Liu <huailun@hotmail.com>
2016-05-25 12:37:42 -0600

    pwp2b

--- public_html/milestone-2b.php
+++ public_html/milestone-2b.php
@@ -4,17 +4,17 @@
 		<meta charset="utf-8">
 
 		<!-- sets IE rendering to IE-EDGE -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 		<!-- sets viewport width to device width, scaling 1:1 -->
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<link rel="css/stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
@@ -34,19 +34,22 @@
 	</head>
 	<body class="sfooter">
 		<div class="sfooter"></div>
-
-		<-- header -->
 		<header>
 			<div class="container">
 				<nav class="navbar navbar-default">
-					<!-- brand and toggle get grouped for better mobile display-->
+
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 						<span class="sr-only">Toggle navigation</span>
 						<span class="icon-bar">About me</span>
 						<p>Hello, this is Ellen. I have started to play piano at age of five and never stopped ever since. From the freshman year at university, I began to give piano lessons. After getting my bachelor degree in piano performance in Taipei, Taiwan, I worked as a piano teacher and piano accompanist. Later, I received my master degree in both piano performance and piano pedagogy from University of Houston in Houston, Texas. I taught class piano, music fundamentals and music appreciation in a community college at Cypress, Texas. I found that no matter in eastern or western world, there are plenty of people looking for learning piano. As a recent career change to be a web-devloper, I hope to incoporate my previous experiences with the new skill I have learned to help people who are interested in learning piano. </p>
 						<span class="icon-bar">About piano</span>
-						<p>Like the golden rule of real estate is "Location, Location, Location", the best way to master in piano is "Practice, Practice, Practice". Because of this, one who is interested in starting a piano lesson, pick the right piano is the important first step. The full size of a standard piano is consist of eighty-eight keys. However, an acoustic piano can cost most people an arm and a leg. My suggestion is to get a weighted electronic keyboard with at least 61 keys .</p>
-						<span class="icon-bar">Some music basics</span>
+						<p>Like the golden rule of real estate is "Location, Location, Location", the best way to master in piano is "Practice, Practice, Practice". Because of this, one who is interested in starting a piano lesson, pick the right piano is the important first step. The full size of a standard piano is consist of eighty-eight keys. However, an acoustic piano can cost most people an arm and a leg. My suggestion is to get a weighted electronic keyboard with at least 61 keys.</p>
+						<span class="icon-bar">Reading Music</span>
+						<p><ul>
+							<li>Tempo</li>
+							<li>Rhythm</li>
+							<li>Treble Clef, Bass Clef and Staff</li>
+						</ul></p>
 						<span class="icon-bar">Link to useful resources</span>
 					</button>
 				</nav>
