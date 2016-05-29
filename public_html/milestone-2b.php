6e0bad218afa8d9901ad128cccd4c5ea23037651
Ellen Liu <huailun@hotmail.com>
2016-05-28 21:51:24 -0600

    pwp2b

--- public_html/milestone-2b.php
+++ public_html/milestone-2b.php
@@ -14,7 +14,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="css/stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
@@ -33,25 +33,68 @@
 		<title>PWP</title>
 	</head>
 	<body class="sfooter">
-		<div class="sfooter"></div>
-		<header>
+		<div class="sfooter-content">
+			<!-- begin bootstrap navbar -->
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>>
+							<span class="icon-bar"></span>>
+							<span class="icon-bar"></span>>
+						</button>
+						<a class="navbar-brand" href="#">Ellen's Online Piano Studio</a>
+					</div><!--/.navbar-header -->
+						<div id="navbar" class="collapse navbar-collapse">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#">Home</a></li>
+								<li><a href="#about-me">About Me</a></li>
+								<li><a href="#about-piano">About Piano</a></li>
+								<li><a href="#reading-music">Reading Music</a></li>>
+								<li><a href="#link">Link</a></li>
+								<li><a href="#contact-me">Contact Me</a></li>
+							</ul>
+						</div><!--/.nar-collapse -->
+					</div>
+			</nav>
 			<div class="container">
-				<nav class="navbar navbar-default">
+				<div class="welcome">
+					<h1>Welcome to Ellen's Online Piano Studio</h1>
+					<p class="lead">Bach gave us God's word.Mozart gave us God's laughter.<br> Beethoven gave us God's fire.<br> God gave us Music that we might pray without words.<br> To many people, music is a powerful part in their personal life. Learning to play an instrument can help people to gain more concentration and release stress. However, from years of teaching and my personal learning experiences, playing piano is not easy. But, if people can start piano lesson in a right path, it can save tons of time and energy and enjoy the happiness of playing piano. Different with other instruments, piano is self-sufficient. It can create melody and chords at the same time. Also, not like other instruments, people don't need to tune piano every time before they play on it. Piano is very helpful to build good music ears.</p>
+				</div>
+			</div><!-- container -->
+			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+			<!-- Include all compiled plugins (below), or include individual files as needed -->
+			<script src="js/bootstrap.min.js"></script>
+								<li class="icon-bar">About me</li>
+								<p>Hello, this is Ellen. I have started to play piano at age of five and never stopped ever since. From the freshman year at university, I began to give piano lessons. After getting my bachelor degree in piano performance in Taipei, Taiwan, I worked as a piano teacher and piano accompanist. Later, I received my master degree in both piano performance and piano pedagogy from University of Houston in Houston, Texas. I taught class piano, music fundamentals and music appreciation in a community college at Cypress, Texas. I found that no matter in eastern or western world, there are plenty of people looking for learning piano. As a recent career change to be a web-devloper, I hope to incoporate my previous experiences with the new skill I have learned to help people who are interested in learning piano. </p>
+								<li class="icon-bar">About piano</li>
+								<p>Like the golden rule of real estate is "Location, Location, Location", the best way to master in piano is "Practice, Practice, Practice". Because of this, one who is interested in starting a piano lesson, pick the right piano is the important first step. The full size of a standard piano is consist of eighty-eight keys. However, an acoustic piano can cost most people an arm and a leg. My suggestion is to get a weighted electronic keyboard with at least 61 keys.After getting a piano,in my opinion, staring from a five-finger position pattern is the most effective way to build a good fundation in piano learning.</p>
+								<li class="icon-bar">Reading Music</li>
+								<ul>
+										<li>Tempo</li>
+								<p>Tempo to music is as important as heart beats to human. Playing music in a steady tempo makes music sound smoothly. Tempo marks of music are in italian. From Larghissimo to Pretissimo means the slowest to the fastest tempo. In another form of expression, tempo can also be marked as BPM - beats per minutes. </p>
+								<li>Rhythm</li>
+								<p>What forms rhythm is the division of the note's value. There are two ways of dividing note's value, diving by two is used more often than diving by three. The different way of division gives music different feeling in its movement.</p>
+								<li>Music Notation: Treble Clef, Bass Clef and Staff</li>
+								<li>Sharps and Flats</li>
+							</ul>
+							</ul>
+						</div>
 
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar">About me</span>
-						<p>Hello, this is Ellen. I have started to play piano at age of five and never stopped ever since. From the freshman year at university, I began to give piano lessons. After getting my bachelor degree in piano performance in Taipei, Taiwan, I worked as a piano teacher and piano accompanist. Later, I received my master degree in both piano performance and piano pedagogy from University of Houston in Houston, Texas. I taught class piano, music fundamentals and music appreciation in a community college at Cypress, Texas. I found that no matter in eastern or western world, there are plenty of people looking for learning piano. As a recent career change to be a web-devloper, I hope to incoporate my previous experiences with the new skill I have learned to help people who are interested in learning piano. </p>
-						<span class="icon-bar">About piano</span>
-						<p>Like the golden rule of real estate is "Location, Location, Location", the best way to master in piano is "Practice, Practice, Practice". Because of this, one who is interested in starting a piano lesson, pick the right piano is the important first step. The full size of a standard piano is consist of eighty-eight keys. However, an acoustic piano can cost most people an arm and a leg. My suggestion is to get a weighted electronic keyboard with at least 61 keys.</p>
-						<span class="icon-bar">Reading Music</span>
-						<p><ul>
-							<li>Tempo</li>
-							<li>Rhythm</li>
-							<li>Treble Clef, Bass Clef and Staff</li>
-						</ul></p>
+
 						<span class="icon-bar">Link to useful resources</span>
+							<ul>
+								<li>IMSLP</li>
+								<li>Online Metronome</li>
+							</ul>
 					</button>
+					</div>
+					</div>
 				</nav>
 			</div>
 		</header>
@@ -79,11 +122,7 @@
 								<h3>Thumbnail label</h3>
 								<p>whatever</p>
 								<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-							</div>
-						</div>
+								</div>
 
-
-					</div>
-
-
+		</div><!--/.sfooter-content -->
 	</Body>
