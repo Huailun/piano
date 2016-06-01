c799f4009151273e550bf9bd6d510dc869de7320
Ellen Liu <huailun@hotmail.com>
2016-05-31 21:43:55 -0600

    pwp2b

--- public_html/milestone-2b.php
+++ public_html/milestone-2b.php
@@ -15,6 +15,10 @@
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- OUR CUSTOM CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
@@ -23,8 +27,6 @@
 		<![endif]-->
 
 
-		<!-- OUR CUSTOM CSS -->
-		<link rel="stylesheet" href="css/style.css" type="text/css">
 
 		<!-- jQuery -->
 		<script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
@@ -34,8 +36,6 @@
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
-			<!-- begin bootstrap navbar -->
-			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="jumbotron">
 				<div class="container">
 					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 pull-right ">
@@ -43,68 +43,69 @@
 					</div>
 					<h1>Bach gave us God's word.<br> Mozart gave us God's laughter.<br> Beethoven gave us God's fire.<br> God gave us Music that we might pray without words.<br></h1>
 					<h2><em>Welcome to Ellen's Online Piano Studio</em></h2>
-					<p><a class="btn btn-primary btn-md" href="#" role="button">Learn more</a></p>
-					</div><!--/.container -->
+					<p><a class="btn btn-primary btn-md" href="#learn" role="button">Learn more</a></p>
+				</div><!--/.container -->
 			</div><!--/.jumbotron -->
-			<nav class="navbar navbar-default navbar-fixed-top navbar-justified">
+			<!-- begin bootstrap navbar -->
+			<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="container">
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 				<div class="container-fluid">
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-							<span class="sr-only">Toggle navigation
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
 							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>>
-							<span class="icon-bar"></span>>
-							<span class="icon-bar"></span>>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Home</a>
+						<a class="navbar-brand" href="#featured">Ellen's Online Piano Studio</a>
 					</div><!--/.navbar-header -->
+					<div class="collapse navbar-collapse" id="collapse">
 					<!-- Collect the nav links and other content for toggling -->
-						<div id="navbar-collapse" class="collapse navbar-collapse">
-							<ul class="nav navbar-nav">
-								<li><a href="#about-me">About Me</a></li>
-								<li class="dropdown">
-									<a href="#about-piano" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-baspopup="true" aria-expanded="false">About Piano<span class="caret"></span></a>
-									<ul class="dropdown-menu">
-										<li>Find the right piano</li>
-										<li>Finger Position</li>
-									</ul>
-								</li><!--/.dropdown for about piano -->
-								<li class="dropdown">
-									<a href="#reading-music" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-baspopup="true" aria-expanded="false">Reading music<span class="caret"></span></a>
-									<ul class="dropdown-menu">
-										<li>Tempo</li>
-										<li>Rhythm</li>
-										<li>Music notation</li>
-										<li>Key signatures</li>
-									</ul>
-								</li><!--/.dropdown for music theory -->
-								<li class="dropdown">
-									<a href="#link" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-baspopup="true" aria-expanded="false">Link<span class="caret"></span></a>
-									<ul class="dropdown-menu">
+						<ul class="nav nav-pills navbar-right">
+							<li class="active"><a href="#featured">Home</a></li>
+							<li><a href="#about-me">About Me</a></li>
+							<li class="dropdown">
+								<a href="#about-piano" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-baspopup="true" aria-expanded="false">About Piano<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li>Find the right piano</li>
+									<li>Finger Position</li>
+								</ul>
+							</li><!--/.dropdown for about piano -->
+							<li class="dropdown">
+								<a href="#reading-music" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-baspopup="true" aria-expanded="false">Reading music<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li>Tempo</li>
+									<li>Rhythm</li>
+									<li>Music notation</li>
+									<li>Key signatures</li>
+								</ul>
+							</li><!--/.dropdown for music theory -->
+							<li class="dropdown">
+								<a href="#link" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-baspopup="true" aria-expanded="false">Link<span class="caret"></span></a>
+								<ul class="dropdown-menu">
 									<li>IMSLP</li>
 									<li>Metronome</li>
-									</ul>
-								</li><!--/.dropdown for link -->
-								<li><a href="#contact-me">Contact Me</a></li>
-							</ul>
-						</div><!--/.nav-collapse -->
-					</div>
+								</ul>
+							</li><!--/.dropdown for link -->
+							<li><a href="#contact-me">Contact Me</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div><!--nav container-->
 			</nav>
 
 			<div class="container">
-				<div class="welcome">
-					<p class="lead">To many people, music is a powerful part in their personal life. Learning to play an instrument can help people to gain more concentration and release stress. However, from years of teaching and my personal learning experiences, playing piano is not easy. But, if people can start their piano lesson in a right path, it can save tons of time and energy and enjoy the happiness of playing piano. Different with other instruments, piano is self-sufficient. It can create melody and chords at the same time. Also, not like other instruments, people don't need to tune their piano every time before they play on it. Piano is also very helpful to build good music ears and understand how music system works.</p>
-				</div>
-			</div><!-- container -->
-			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-			<!-- Include all compiled plugins (below), or include individual files as needed -->
-			<script src="js/bootstrap.min.js"></script>
-								<li class="icon-bar">About me</li>
+					<p class="lead" id="learn">To many people, music is a powerful part in their personal life. Learning to play an instrument can help people to gain more concentration and release stress. However, from years of teaching and my personal learning experiences, playing piano is not easy. But, if people can start their piano lesson in a right path, it can save tons of time and energy and enjoy the happiness of playing piano. Different with other instruments, piano is self-sufficient. It can create melody and chords at the same time. Also, not like other instruments, people don't need to tune their piano every time before they play on it. Piano is also very helpful to build good music ears and understand how music system works.</p>
+					<img class="piano" src="images/mypiano.jpg" alt="my piano">
+			</div><!--container-->
+				<h2>About me</h2>
+					<div class="row">
+						<div class="col-sm-12">
 								<p>Hello, this is Ellen. I have started to play piano at age of five and never stopped ever since. From the freshman year at university, I began to give piano lessons. After getting my bachelor degree in piano performance in Taipei, Taiwan, I worked as a piano teacher and collaborative pianist. Later, I received my master degree in both piano performance and piano pedagogy from University of Houston in Houston, Texas. I taught class piano, music fundamentals and music appreciation in a community college at Cypress, Texas. I found that no matter in eastern or western world, there are plenty of people looking for learning piano. As a recent career change to be a web-developer, I hope to incorporate my previous experiences with the new skill I have learned to help people who are interested in learning piano. </p>
-								<li class="icon-bar">About piano</li>
-								<p>Like the golden rule of real estate is "Location, Location, Location", the best way to master in piano is "Practice, Practice, Practice". Because of this, one who is interested in starting a piano lesson, pick the right piano is the important first step. The full size of a standard piano is consist of eighty-eight keys. However, an acoustic piano can cost most people an arm and a leg. My suggestion is to get a weighted electronic keyboard with at least 61 keys.After getting a piano,in my opinion, staring from a five-finger position pattern is the most effective way to build a good fundation in piano learning.</p>
-								<li class="icon-bar">Reading Music</li>
+			</div><!--/. container of about me -->
+								<h2>About piano</h2>
+								<p id="find_the_right_piano">Like the golden rule of real estate is "Location, Location, Location", the best way to master in piano is "Practice, Practice, Practice". Because of this, one who is interested in starting a piano lesson, pick the right piano is the important first step. The full size of a standard piano is consist of eighty-eight keys. However, an acoustic piano can cost most people an arm and a leg. My suggestion is to get a weighted electronic keyboard with at least 61 keys.After getting a piano,in my opinion, staring from a five-finger position pattern is the most effective way to build a good fundation in piano learning.</p>
+								<h2>Reading Music</h2>
 								<ul>
 										<li>Tempo</li>
 										<p>Tempo to music is as important as heart beats to human. Playing music in a steady tempo makes music sound smoothly. Tempo marks of music are in italian. From Larghissimo to Pretissimo means the slowest to the fastest tempo. In another form of expression, tempo can also be marked as BPM - beats per minutes. </p>
@@ -120,9 +121,7 @@
 
 					</button>
 					</div>
-					</div>
 				</nav>
-			</div>
 		<form class="form-horizontal" role="form" method="post" action="index.php">
 			<div class="form-group">
 				<label for="name" class="col-sm-2 control-label">Name</label>
@@ -159,30 +158,9 @@
 				</div>
 			</div>
 		</form>
-		</header>
-		<!-- welcome section -->
-		<section = id="welcome">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-3">
-					image
-				</div>
-			</div>
-		</div>
-		</section>
-
-		<!-- thumbnails -->
-		<section id="thumbnails">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-3">
-						<div class="thumbnail">
-							<img src="http:://placekitten.com/g/100/100" alt="" class="image">
-							<div class="caption">
-								<h3>Thumbnail label</h3>
-								<p>whatever</p>
-								<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-								</div>
-
 		</div><!--/.sfooter-content -->
-	</Body>
+		<footer>
+			<p>copyright</p>
+		</footer>
+	</div><!-- container-->
+	</body>
