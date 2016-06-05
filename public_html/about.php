aa6551bd52254a4dde9e04bb708692d246ee2f55
Ellen Liu <huailun@hotmail.com>
2016-06-04 20:06:40 -0600

    pwp2b

--- public_html/about.php
+++ public_html/about.php
@@ -117,7 +117,7 @@
 				<img class="roland img-responsive" src="images/roland.jpg" alt="keyboard">
 				<p id="finger" class="finger_position"><strong>Finger position</strong><br>In my opinion, starting directly from the five-finger position is the most effective way to
 					build a good foundation for learning. As all piano music requires the ten fingers to work together to
-					create fine music, it is very important to have equal strength in all fingers(finger independence). Practicing with all fingers from the beginning will prepare you to play more complex pieces and simplify the path to mastering the piano. Please see
+					create fine music, it is very important to have equal strength in all fingers (finger independence). Practicing with all fingers from the beginning will prepare you to play more complex pieces and simplify the path to mastering the piano. Please see
 					the picture below, which shows the numbers assigned to each of the fingers. Once you've memorized these, take a look at the
 					keyboard image above this paragraph. Notice the arrangement of the black keys: it is a two-three-two-three
 					pattern. Surrounding each group of two black keys group are three white keys. From left to right, these are the notes C, D, and E. The four white keys
